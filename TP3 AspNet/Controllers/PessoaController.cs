2982e0e4b3a269eed5bf2a690df1110ddddd247c
Lucas Samel <lucas.samel97@gmail.com>
2020-05-21 22:12:07 -0300

    Add project files.

---
+++ TP3 AspNet/Controllers/PessoaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TP3_AspNet.Controllers
+{
+    public class PessoaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
